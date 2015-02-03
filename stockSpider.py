d34c978eb836d47673fcae44c9280a9ced1932a6
MuerteLee <378153740@qq.com>
2015-02-03 22:46:03 +0800

    add update key world

--- stockSpider.py
+++ stockSpider.py
@@ -11,7 +11,7 @@ class sqliteOS3(object):
         if not os.path.isfile(dataBasePath):
            conn = sqlite3.connect(dataBasePath) 
            cu = conn.cursor()
-           cu.execute("create table stockSpider(id integer primary key autoincrement, symbol varchar(128) UNIQUE, name varchar(128) UNIQUE, cnt_r0x_ratio varchar(128), trade varchar(128), changeratio varchar(128), turnover varchar(128), amount varchar(128), netamount varchar(128), ratioamount varchar(128), r0_net varchar(128), r0x_ratio varchar(128))")
+           cu.execute("create table stockSpider(id integer primary key autoincrement, symbol varchar(128) UNIQUE, name varchar(128) UNIQUE, cnt_r0x_ratio varchar(128), trade varchar(128), changeratio varchar(128), turnover varchar(128), amount varchar(128), netamount varchar(128), ratioamount varchar(128), r0_net varchar(128), r0x_ratio varchar(128), updateKey integer)")
            conn.close()  
 
         self.dataBasePath = dataBasePath;
@@ -19,7 +19,7 @@ class sqliteOS3(object):
     def connectData(self,):
         return sqlite3.connect(self.dataBasePath); 
 
-    def insertData(self, conn, symbol, name, cnt_r0x_ratio, trade, changeratio, turnover, amount, netamount, ratioamount, r0_net, r0x_ratio):
+    def insertData(self, conn, symbol, name, cnt_r0x_ratio, trade, changeratio, turnover, amount, netamount, ratioamount, r0_net, r0x_ratio, updateKey):
         self.symbol = symbol;
         self.name = name;
         self.cnt_r0x_ratio = cnt_r0x_ratio;
@@ -31,9 +31,10 @@ class sqliteOS3(object):
         self.ratioamount = ratioamount;
         self.r0_net = r0_net;
         self.r0x_ratio = r0x_ratio;
+        self.updateKey = updateKey;
 
         cu = conn.cursor(); 
-        cmdLineInsert = "insert into stockSpider values((select max(ID) from stockSpider)+1," + self.symbol + "," + self.name + "," + self.cnt_r0x_ratio + "," + self.trade + "," + self.changeratio + "," + self.turnover + "," + self.amount + "," + self.netamount + "," + self.ratioamount + "," + self.r0_net + "," + self.r0x_ratio + ")"
+        cmdLineInsert = "insert into stockSpider values((select max(ID) from stockSpider)+1," + self.symbol + "," + self.name + "," + self.cnt_r0x_ratio + "," + self.trade + "," + self.changeratio + "," + self.turnover + "," + self.amount + "," + self.netamount + "," + self.ratioamount + "," + self.r0_net + "," + self.r0x_ratio + "," + self.updateKey + ")"
         print("Update new data: %s" %cmdLineInsert)
         cu.execute(cmdLineInsert)
         conn.commit()
@@ -100,6 +101,7 @@ class parseUrl(sqliteOS3):
         data = opener.open(URL).read()
         self.mainData = data.decode("GB2312").strip('[').strip(']').replace('},','}},').split('},');
         self.dataBasePath = dataBasePath;
+        self.updateKeyValue = 0
 
     def optionData(self,):
         sql = sqliteOS3(self.dataBasePath);
@@ -107,17 +109,17 @@ class parseUrl(sqliteOS3):
         for i in range(0, len(self.mainData)):
             list =eval((self.mainData[i]).replace('{','{"').replace(':', '":').replace(',',',"'))
             if sql.searchStockSpiderSqlite3(conn, list['symbol']):
-                sql.insertData(conn, '"'+list['symbol']+'"', '"'+list['name']+'"', list['cnt_r0x_ratio'], list['trade'], list['changeratio'], list['turnover'], list['amount'], list['netamount'],list['ratioamount'], str(locale.atof(list['r0_net'])), str(locale.atof(list['r0x_ratio'])))
+                sql.insertData(conn, '"'+list['symbol']+'"', '"'+list['name']+'"', list['cnt_r0x_ratio'], list['trade'], list['changeratio'], list['turnover'], list['amount'], list['netamount'],list['ratioamount'], str(locale.atof(list['r0_net'])), str(locale.atof(list['r0x_ratio'])), str(self.updateKeyValue))
             if locale.atof(list['amount']) != locale.atof(sql.searchStockSpiderAmount(conn, list['symbol'])):
                 if sql.deleteUpdateData(conn,list['symbol']):
-                    sql.insertData(conn, '"'+list['symbol']+'"', '"'+list['name']+'"', list['cnt_r0x_ratio'], list['trade'], list['changeratio'], list['turnover'], list['amount'], list['netamount'],list['ratioamount'], str(locale.atof(list['r0_net'])), str(locale.atof(list['r0x_ratio'])))
-#                sql.updateStockSpiderData(conn,list['symbol'], list['cnt_r0x_ratio'], list['trade'], list['changeratio'], list['turnover'], list['amount'], list['netamount'],list['ratioamount'], list['r0_net'], list['r0x_ratio']);
+                    self.updateKeyValue = self.updateKeyValue + 1
+                    sql.insertData(conn, '"'+list['symbol']+'"', '"'+list['name']+'"', list['cnt_r0x_ratio'], list['trade'], list['changeratio'], list['turnover'], list['amount'], list['netamount'],list['ratioamount'], str(locale.atof(list['r0_net'])), str(locale.atof(list['r0x_ratio'])), str(self.updateKeyValue))
 
         sql.closeSqlite3(conn); 
 if __name__ == "__main__":
 #    parseUrl("http://data.eastmoney.com/bkzj/hy.html");
     dataBasePath = "/home/muerte/stockSpider.db"
-    for pageNum in range(1, 13):
+    for pageNum in range(1, 132):
         url = "http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/MoneyFlow.ssl_bkzj_lxjlr?page="+str(pageNum)+"&num=20&sort=cnt_r0x_ratio&asc=0&bankuai="
         print("Parese new URL%d: %s" %(pageNum,url))
         stock = parseUrl(url, dataBasePath);
