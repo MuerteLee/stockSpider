ec57ca54b9eea44cb7b1d244bcf45cc1b4c14c97
MuerteLee <378153740@qq.com>
2015-03-05 22:45:50 +0800

    add insert data what each of stock

--- stockSymbol.py
+++ stockSymbol.py
@@ -3,6 +3,43 @@ from urllib import request
 import sqlite3
 import re, time,os 
 
+class dataBaseOperator(object):
+    def __init__(self,conn, tableName):
+        self.conn = conn;
+        self.tableName =  tableName
+        self.cur = self.conn.cursor()
+
+    def searchStockTable(self,):
+        cmdLine = "SELECT COUNT(*) FROM sqlite_master where type='table' and name=" + "'" + self.tableName + "'"
+        try:
+            self.cur.execute(cmdLine)
+            lenS = int (str(self.cur.fetchall()[0]).replace('(','').replace(",)",""))
+            if lenS == 1:
+                return True;
+            else:
+                print("False\n")
+                return False;
+
+        except sqlite3.Error as e:
+            print("ERROR: searchStockTable error in the dataBaseOperator, please check your param! \n")
+            return False;
+
+    def insertMainTableData(self, name, priceCur, price41MH, price46MH, price452WH, price452WL):
+        self.stockID = self.tableName;
+        self.name = name;
+        self.priceCur = priceCur;
+        self.price41MH = price41MH;
+        self.price46MH = price46MH;
+        self.price452WH = price452WH;
+        self.price452WL = price452WL;
+
+        cmdLineInsert = "insert into stockMainTable values((select max(ID) from stockMainTable)+1," + '"' + self.stockID + '"' + "," + '"' + self.name+'"' + "," + '"'+ self.priceCur + '"' + "," + '"' + self.price41MH + '"' + "," + '"' + self.price46MH + '"' + "," + '"' + self.price452WH + '"' + "," + '"' + self.price452WL + '"' + ")"
+        print("Update new data: %s" %cmdLineInsert)
+        self.cu.execute(cmdLineInsert)
+        self.conn.commit()
+
+
+
 class dataBase(object):
     def __init__(self, dataBasePath):
         if not os.path.isfile(dataBasePath):
@@ -15,20 +52,6 @@ class dataBase(object):
     def connectData(self,):
         return sqlite3.connect(self.dataBasePath); 
 
-    def insertMainTableData(self, conn, stockID, name, priceCur, price41MH, price46MH, price452WH, price452WL):
-        self.stockID = stockID;
-        self.name = name;
-        self.priceCur = priceCur;
-        self.price41MH = price41MH;
-        self.price46MH = price46MH;
-        self.price452WH = price452WH;
-        self.price452WL = price452WL;
-
-        cu = conn.cursor(); 
-        cmdLineInsert = "insert into stockMainTable values((select max(ID) from stockMainTable)+1," + '"' + self.stockID + '"' + "," + '"' + self.name+'"' + "," + '"'+ self.priceCur + '"' + "," + '"' + self.price41MH + '"' + "," + '"' + self.price46MH + '"' + "," + '"' + self.price452WH + '"' + "," + '"' + self.price452WL + '"' + ")"
-        print("Update new data: %s" %cmdLineInsert)
-        cu.execute(cmdLineInsert)
-        conn.commit()
 
     def searchStockFilterSqlite3(self,conn, stockID):
         cur = conn.cursor()
@@ -41,6 +64,33 @@ class dataBase(object):
         except sqlite3.Error as e:
             print("ERROR: searchStockFilterSqlite3 error, please check your param! \n")
             return False;
+
+class dataBase4Stock(object):
+    def __init__(self, dataBasePath,stockSymbol):
+        dataBase.__init__(self, dataBasePath);
+        self.dataBasePath = dataBasePath;
+        self.stockSymbol = stockSymbol
+        sql = dataBase(self.dataBasePath) 
+        conn = sql.connectData()
+    
+        if not dataBaseOperator(conn, stockSymbol).searchStockTable(): 
+           cu = conn.cursor()
+           cmdLine = "create table " + self.stockSymbol + "(id integer primary key autoincrement, time varchar(128) UNIQUE, price float)"
+           print(cmdLine)
+           try:
+                cu.execute(cmdLine)
+           except sqlite3.Error as e:
+                print("Please check dataBase4Stock!\n")
+           conn.close()  
+
+    def insert4Stock(self, date, price):
+        self.date = date;
+        self.price = price;
+
+        cmdLineInsert = "insert into " + self.stockSymbol +" values((select max(ID) from " + self.stockSymbol + ")+1," + '"' + self.date + '"' + "," + '"' + self.price + ")"
+        print("Insert stock data: %s" %cmdLineInsert)
+        self.cu.execute(cmdLineInsert)
+        self.conn.commit()
 
 class option(dataBase):
     def __init__(self, data, dataBasePath):
@@ -55,7 +105,7 @@ class option(dataBase):
             if self.data[2] == '-':
                 self.data[2] = '0.0'
 #            sql.insertMainTableData(conn, str(self.data[0]), self.data[1], self.data[2], str(self.data[4]), str(self.data[5]), str(self.data[6]), str(self.data[7]));
-            sql.insertMainTableData(conn, str(self.data[0]), self.data[1], self.data[2], "0", "0", "0", "0");
+            dataBaseOperator(conn, str(self.data[0])).insertMainTableData(self.data[1], self.data[2], "0", "0", "0", "0");
 
 class parseUrl(object):
     def __init__(self, URL):
@@ -75,7 +125,7 @@ class parseUrl(object):
         return self.data;
 
 if __name__ == "__main__":
-    dataBasePath = "./.stockMainTable.db"
+    dataBasePath = "./.stockMain.db"
     url = "http://nufm.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx/JS.aspx?type=ct&st=%28BalFlowMain%29&sr=-1&p=1&ps=50&js=var%20pglwSLMJ={pages:%28pc%29,date:%222014-10-22%22,data:%5B%28x%29%5D}&token=894050c76af8597a853f5b408b759f5d&cmd=C._AB&sty=DCFFITA&rt=47466059"
     urlS = "http://nufm.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx/JS.aspx?type=ct&st=%28BalFlowMain%29&sr=-1&p="
     urlE = "&ps=50&js=var%20pglwSLMJ={pages:%28pc%29,date:%222014-10-22%22,data:%5B%28x%29%5D}&token=894050c76af8597a853f5b408b759f5d&cmd=C._AB&sty=DCFFITA&rt=47466059"
@@ -103,5 +153,10 @@ if __name__ == "__main__":
  
             del parseDataTmp[0]
 
-            print(parseDataTmp)
             option(parseDataTmp,dataBasePath).insertStockData();
+
+            sql = dataBase(dataBasePath) 
+            conn = sql.connectData()
+            stockSymbol = parseDataTmp[0]
+
+            dataBase4Stock(dataBasePath, stockSymbol)
